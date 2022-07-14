8f73ee859affb2a724784a5e52d14c2b5c57ce2d
Carlos Guizar Tejeda <cguizar@users.noreply.github.com>
2022-07-14 15:47:51 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(400,400);
+    canvas.center();
+    Video = createCapture(VIDEO);
+    Video.size(300,300);
+    Video.center();
+}
+
+function draw() {
+    
+}
